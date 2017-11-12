8705b90f9d218dcce69751eceab9a80a54640dd6
alan.price <alan@theprices.us>
2017-11-12 14:26:54 -0600

    Cleaned up Vue Pipeline.

--- src/VuePipeline.js
+++ src/VuePipeline.js
@@ -8,22 +8,53 @@ export class VuePipeline {
         if (this.filePath.indexOf('.vue') === -1) {
             return this.fullImportText
         }
-        let styleImport = '\n`;\nconst styleTag = document.createElement("style");\nstyleTag.innerHTML = styles;\ndocument.head.appendChild(styleTag)'
-        let converted = this.fullImportText.replace('<template>', 'const template = `').replace('</template>', '`')
-        converted = converted.replace('<style>', 'let styles = `').replace('</style>', styleImport)
-
+        let converted = this.processTemplate(this.fullImportText)
         if (converted.indexOf('<script src=') > -1) {
-            let [firstHalf, secondHalf] = converted.split('<script src=')
-            converted = firstHalf + '\nimport component from ' + secondHalf.replace('>', '')
-            converted = converted + 'component.template = template;\nexport default component'
-            converted = converted.replace('<script>', '').replace('</script>', '')
-            return converted
-        } else {
-            converted = converted.replace('<script>', '').replace('</script>', '')
-            if (converted.indexOf('export default {')) {
-                return converted.replace('export default {', 'export default {\n template: template,')
-            }
-            return converted.replace('data()', 'template: template,\ndata()')                
+            return this.convertSrcScript(converted)
         }
+        return this.convertScript(converted)
+    }
+
+    /**
+     * Process Template
+     * 
+     * @param {string} converted 
+     * @returns {string}
+     * @memberof VuePipeline
+     */
+    processTemplate(converted) {
+        let styleImport = '\n`;\nconst styleTag = document.createElement("style");\nstyleTag.innerHTML = styles;\ndocument.head.appendChild(styleTag)'
+        converted = this.fullImportText.replace('<template>', 'const template = `').replace('</template>', '`')
+        return converted.replace('<style>', 'let styles = `').replace('</style>', styleImport)
+    }
+
+    /**
+     * Convert Source Script
+     * 
+     * @param {string} converted 
+     * @returns {string}
+     * @memberof VuePipeline
+     */
+    convertSrcScript(converted) {
+        let [firstHalf, secondHalf] = converted.split('<script src=')
+        converted = firstHalf + '\nimport component from ' + secondHalf.replace('>', '')
+        converted = converted + 'component.template = template;\nexport default component'
+        converted = converted.replace('<script>', '').replace('</script>', '')
+        return converted
+    }
+
+    /**
+     * Convert Script
+     * 
+     * @param {any} converted 
+     * @returns 
+     * @memberof VuePipeline
+     */
+    convertScript(converted) {
+        converted = converted.replace('<script>', '').replace('</script>', '')
+        if (converted.indexOf('export default {')) {
+            return converted.replace('export default {', 'export default {\n template: template,')
+        }
+        return converted.replace('data()', 'template: template,\ndata()')      
     }
 }
